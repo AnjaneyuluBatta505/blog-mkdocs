6e538b644d783501d116af4d446b481e3c8ae938
Anjaneyulu Batta <anjaneyulu.batta505@gmail.com>
2020-03-14 01:11:21 +0530

    related blog articles added

--- blog/plugin.py
+++ blog/plugin.py
@@ -1,16 +1,18 @@
+import re
 from mkdocs.utils import meta
 from mkdocs.plugins import BasePlugin
 
 
 class Blog(BasePlugin):
 
-    def change_dest_path_for_blog(self, file_obj):
-        abs_dest_path = file_obj.abs_dest_path
-        abs_dest_path = re.sub(r'\d{4}-\d{2}-\d{2}\_', '', abs_dest_path)
-        file_obj.abs_dest_path = abs_dest_path
-        dest_path = re.sub(r'\d{4}-\d{2}-\d{2}\_', '', dest_path)
-        file_obj.dest_path = dest_path
-        print("*" * 10, file_obj.abs_dest_path, file_obj.dest_path)
+    def on_files(self, files, config):
+        for file_obj in files:
+            abs_dest_path = file_obj.abs_dest_path
+            abs_dest_path = re.sub(r'\d{4}-\d{2}-\d{2}\_', '', abs_dest_path)
+            file_obj.abs_dest_path = abs_dest_path
+            dest_path = file_obj.dest_path
+            dest_path = re.sub(r'\d{4}-\d{2}-\d{2}\_', '', dest_path)
+            file_obj.dest_path = dest_path
 
     def get_meta_data(self, path):
         with open(path, 'r') as f:
@@ -25,17 +27,20 @@ class Blog(BasePlugin):
         sort_key = 'date_published'
         blog_articles = sorted(blog_data, key=lambda obj: obj[sort_key], reverse=True)
         return blog_articles[:10]
+    
+    def get_related_blog_articles(self, page, blog_data):
+        rel_articles = page.meta.get('related_articles', [])
+        return tuple(filter(lambda x: x.get('title') in rel_articles, blog_data))
 
-    def on_page_context(self, context, **kwargs):
+    def on_page_context(self, context, page, config, nav, **kwargs):
         blog_data = []
         pages = context['pages']
         for file_obj in pages:
             if file_obj.src_path.startswith('blog/'):
-                self.change_dest_path_for_blog(file_obj)
                 meta_data = self.get_meta_data(file_obj.abs_src_path)
                 blog_data.append(meta_data)
         context['latest_blog_artices'] = self.get_latest_blog_artices(
             blog_data)
-        print("#" * 100)
-        print(context['latest_blog_artices'])
+        rel_articles = self.get_related_blog_articles(page, blog_data)
+        context['related_articles'] = rel_articles
         return context
